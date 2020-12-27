88c0e9236405c3747c26c4c7a9f6564702f7afef
worsrp <sirapop_p@cmu.ac.th>
2020-12-28 02:42:38 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,97 @@
 import { useState } from "react";
-import { CourseCard } from "./components";
+import {CourseCard} from "./components/CourseCard";
+
 function App() {
   const grade = ["A", "B+", "B", "C+", "C", "D+", "D", "F", "W"];
   const credit = [1, 2, 3];
 
   const [myCourses, setMyCourse] = useState([]);
-  const [inputData, setInputData] = useState({});
+  const [inputData, setInputData] = useState({name:"",grd:"A",crd:"1"});
   const [GPA, setGPA] = useState(4.0);
 
   /**
    * Calculate the GPA of current courses
    * @returns the GPA of current courses
    */
-  function calculateGPA() {
+  function calculateGPA(cc) {
     // TODO
+    var gpa = 0
+    var total_credit  = 0 
+    var total_gpa = 0
+    cc.forEach((item) => {
+      switch(item.grd){
+        case 'A' :
+          gpa = 4
+          total_credit += Number(item.crd) 
+          total_gpa += gpa * Number(item.crd)
+          break
+        case 'B+' :
+          gpa = 3.5
+          total_credit += Number(item.crd)
+          total_gpa += gpa * Number(item.crd)
+          break
+        case 'B' :
+          gpa = 3
+          total_credit += Number(item.crd)
+          total_gpa += gpa * Number(item.crd)
+          break
+        case 'C+' :
+          gpa = 2.5
+          total_credit += Number(item.crd)
+          total_gpa += gpa * Number(item.crd)
+          break
+        case 'C' :
+          gpa = 2
+          total_credit += Number(item.crd)
+          total_gpa += gpa * Number(item.crd)
+          break
+        case 'D+' :
+          gpa = 1.5
+          total_credit += Number(item.crd)
+          total_gpa += gpa * Number(item.crd)
+          break
+        case 'D' :
+          gpa = 1
+          total_credit += Number(item.crd)
+          total_gpa += gpa * Number(item.crd)
+          break
+        case 'F' :
+          gpa = 0
+          total_credit += Number(item.crd)
+          total_gpa += gpa * Number(item.crd)
+          break
+      }  
+    });
+    setGPA(total_gpa / total_credit) 
   }
 
   /**
    * Should be called when a course is to be added to the list.
    * After adding the course to the list, the displayed GPA should be updated.
-   * @param {*} event
+   * @param {*} event 
    */
   function addCourse(event) {
     event.preventDefault();
     // TODO
-
+    console.log(inputData.name)
+    const course = [...myCourses,inputData]
+    setMyCourse(course)
     // recalculate GPA
-    calculateGPA();
+    calculateGPA(course);
   }
 
   /**
    * Should be called when a course is to be removed from the list.
    * After removing the course from the list, the displayed GPA should be updated.
-   * @param {*} id
+   * @param {*} id 
    */
   function onDeleteCourse(id) {
     // TODO
+    const course = myCourses.filter(item => {
+      return item.name !== id
+    })
+    setMyCourse(course)
+    calculateGPA(course)
   }
 
   return (
@@ -44,8 +100,45 @@ function App() {
         GPA CALCULATOR
       </h1>
       <div className="h-2/3 md:w-2/4 p-3 rounded-lg mx-auto overflow-auto">
-        <h1 className="text-2xl my-3">My courses</h1>
+        <section>
+        <article><h1 className="text-2xl my-3">My courses</h1></article>
+        <article class="result"><p>GPAX</p>{GPA}</article>
+        </section>
         {/* TODO display courses */}
+        <section>
+          <article>
+            <div><b>Add course</b></div>
+            Credit : 
+        <select onChange = { e => 
+            setInputData({...inputData,crd: e.currentTarget.value}) 
+          }>
+          {credit.map(item => {
+            return <option value={item}>{item}</option>
+          })}
+        </select>      
+          GPA :
+        <select onChange = { e => 
+           setInputData({...inputData,grd: e.currentTarget.value}) 
+          }>
+          {grade.map(item => {
+            return <option value={item}>{item}</option>
+          })}
+        </select>  
+        </article>
+        <article>
+        Class name :
+        <form onSubmit ={
+          e => addCourse(e)}>
+          <input type="text" onChange = { e => 
+            setInputData({...inputData,name: e.currentTarget.value}) 
+          }/>
+            <button type="submit" class="button1">+</button>
+       </form>
+       </article>
+      </section>
+        {myCourses.map(item => {
+          return <CourseCard name ={item.name} grd = {item.grd} crd ={item.crd} del={onDeleteCourse} />
+        })}
       </div>
       {/* TODO add course input form */}
       {/* TODO display calculated GPA */}
--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,13 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return <>
+  <p>
+  <section class="result">
+      <article>Class Name: {props.name}</article>
+      <nav>GPA : {props.grd}</nav>
+      <nav>Credits : {props.crd}</nav>
+      <nav class="tail"><button onClick = {() =>props.del(props.name)} > x </button></nav>
+   </section>
+   </p>
+  </>;
 };
